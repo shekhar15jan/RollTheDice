1a0e430a0ec5ef1e9e193e27a75fe406cef95186
Chandrashekhar Nandanwar <shekhar15jan@gmail.com>
2022-04-03 20:11:10 +0530

    Update Dice Image Commit

--- app/src/main/java/com/trainings/rollthedice/MainActivity.kt
+++ app/src/main/java/com/trainings/rollthedice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.trainings.rollthedice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.btnRollDice)
-        rollButton.setOnClickListener{ rollDice() }
+        rollButton.setOnClickListener { rollDice() }
     }
 
     /**
@@ -22,16 +22,26 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         val dice = Dice(6)
-        val textVieRollResult: TextView = findViewById(R.id.textRollResult)
-        textVieRollResult.text = dice.roll().toString()
+        val rollResult = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView4)
+
+        val diceRoll = when (rollResult) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(diceRoll)
     }
 }
 
 /**
  * Dice works on multiple sides
  */
-class Dice(private val numSides: Int){
-    fun roll(): Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
